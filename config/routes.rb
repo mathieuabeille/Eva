57232e5b4fff3071edaaa3f9fb3494ebc02f0394
Mathieu <mathieu.abeille@gmail.com>
2018-06-20 21:31:19 +0200

    v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,26 @@
 Rails.application.routes.draw do
   resources :salons
   resources :annonceurs
- devise_scope :user do
-    get "/sign_in" => "devise/sessions#new", as: "new_user_session" # custom path to login/sign_in
-    # post "/sign_in" => "devise/sessions#create", as: "user_session" # custom path to login/sign_in
-    delete "/sign_out" => "devise/sessions#destroy", as: "destroy_user_session"
-    get "/B21zukt938lama272kutoni" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
-    post "/B21zukt938lama272kutoni" => "devise/registrations#create", as: "user_registration" # custom path to login/sign_in
-  end
+
+devise_for :users, skip: [:registrations]
+as :user do
+  get 'B21zukt938lama272kutoni', to: 'devise/registrations#new', as: :new_user_registration
+  post 'B21zukt938lama272kutoni', to: 'devise/registrations#create', as: :user_registration
+end
+
+
+
+
+
+
+#  devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations", confirmations: "users/confirmations", passwords: "users/passwords" }, :skip => [:sessions]
+# devise_scope :user do
+#     get "/sign_in" => "devise/sessions#new", as: "new_user_session" # custom path to login/sign_in
+#     post "/sign_in" => "devise/sessions#create", as: "user_session" # custom path to login/sign_in
+#     match "/sign_out" => "devise/sessions#destroy", as: "destroy_user_session", :via => Devise.mappings[:user].sign_out_via
+#     get "/B21zukt938lama272kutoni" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+#     post "/B21zukt938lama272kutoni" => "devise/registrations#create", as: "user_registration" # custom path to login/sign_in
+#   end
 
   # Below for all other routes:
 
