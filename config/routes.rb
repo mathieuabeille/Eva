616c8278f9f4a2063f9a48735b28ac357e2b9404
Mathieu <mathieu.abeille@gmail.com>
2018-06-20 19:40:48 +0200

    v2

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :annonceurs
  devise_scope :user do
     get "/sign_in" => "devise/sessions#new", as: "new_user_session" # custom path to login/sign_in
-    post "/sign_in" => "devise/sessions#create", as: "user_session" # custom path to login/sign_in
-    get "/sign_out" => "devise/sessions#destroy", as: "destroy_user_session"
+    # post "/sign_in" => "devise/sessions#create", as: "user_session" # custom path to login/sign_in
+    delete "/sign_out" => "devise/sessions#destroy", as: "destroy_user_session"
     get "/B21zukt938lama272kutoni" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
     post "/B21zukt938lama272kutoni" => "devise/registrations#create", as: "user_registration" # custom path to login/sign_in
   end
